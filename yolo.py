31f0c32b4352bdf7d68b93ed79c7596e4bf626f6
d-ly-h <938721706@qq.com>
2026-08-26 06:32:07 +0800

    模拟yolo获取数据

--- yolo.py
+++ yolo.py
@@ -1,6 +1,7 @@
 #模拟yolo8n.pt获取信息
 
 dets = [
+    {"cls": "blue_cube", "cx": 320.0, "cy": 240.0, "conf": 0.95},
     {"cls": "green_cube", "cx": 480.0, "cy": 240.0, "conf": 0.93},
     {"cls": "orange_body", "cx": 100.0, "cy": 180.0, "conf": 0.87},
 ]
